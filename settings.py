32d90d63c468a4d3f18366797aac54fe2cf3bf47
adokmanovic <alen.dokmanovic@mai.hr>
2020-07-29 20:20:10 +0100

    Initial commit

---
+++ settings.py
@@ -0,0 +1,49 @@
+class Settings:
+    """klasa u koju ćemo spremati postavke za najezdu svemiraca"""
+
+    def __init__(self):
+        """inicijalizira postavke igre"""
+
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
+        self.ship_limit = 3
+
+        # Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 300
+        self.bullet_height = 15
+        self.bullet_color = (255, 0, 0)
+        self.bullets_allowed = 500
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 50
+        # fleet direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+        # how quickly game speeds up
+        self.speedup_scale = 1.1
+        # how quickly alien point value increase
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """initialize settings that change throughout the game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+        self.fleet_direction = 1
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings and points value."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
---
+++ ship.py
@@ -0,0 +1,44 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Ship(Sprite):
+    """klasa za brod"""
+
+    def __init__(self, ai_game):
+        """inicijalizira brod i postavljea početni položaj"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # load slike broda and get its rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+        # Store a decimal value for the ships horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Updae ships position based on the movement flag."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Drav the ship at its current location"""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
